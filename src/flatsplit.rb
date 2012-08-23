cff088cf4a2b91abf1587cd0480895687e5c24c1
Tomoaki Nishiyama <tomoaki@staff.kanazawa-u.ac.jp>
2012-08-23 17:46:28 +0900

    more proper handling of directory

--- src/flatsplit.rb
+++ src/flatsplit.rb
@@ -12,13 +12,13 @@ while fe = ff.next_entry
   if filecount % maxdir == 0 && count % maxcount == 0
     Dir.chdir(owd)
     dircount += 1
-    newdir = (ARGV[0] + "." + dircount.to_s)
+    newdir = ("#{File.basename(ARGV[0])}.#{dircount.to_s}")
     Dir.mkdir(newdir)
     Dir.chdir(newdir)
   end
   if count == maxcount then
     filecount += 1
-    of=open(ARGV[0]+"."+filecount.to_s,"w")
+    of=open("#{File.basename(ARGV[0])}.#{filecount.to_s}","w")
     count = 0
   end
   of.print fe.to_s
