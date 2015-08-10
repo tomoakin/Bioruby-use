9a8fb052b156dfb678875595a96a0ca52e9c520e
Tomoaki NISHIYAMA <tomoakin@kenroku.kanazawa-u.ac.jp>
2015-08-10 16:57:08 +0900

    skip entries without entry_id, which is sometimes seen at the end of genbank file

--- src/gc_wiggle.rb
+++ src/gc_wiggle.rb
@@ -8,8 +8,11 @@ window_size = 5000 if window_size == nil or window_size < 1
 
 ff = Bio::FlatFile.open(nil,ARGV[0])
 ff.each do |fe|
+  next if fe.entry_id == nil
+  entry = fe.acc_version
+  entry = fe.entry_id if entry == nil
   cur_win = 1
-  puts "fixedStep chrom=#{fe.acc_version} start=#{cur_win} step=#{window_size}"
+  puts "fixedStep chrom=#{entry} start=#{cur_win} step=#{window_size}"
   fe.naseq.window_search(window_size,window_size) do |winseq|
     puts winseq.gc_content.to_f * 100
     cur_win += window_size   
