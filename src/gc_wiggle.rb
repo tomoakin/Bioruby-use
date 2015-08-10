da8e2fcb58307692c8fb98d4d1b10f51777a33ec
Tomoaki NISHIYAMA <tomoakin@kenroku.kanazawa-u.ac.jp>
2015-08-10 15:31:52 +0900

    gc content write to wig format

---
+++ src/gc_wiggle.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+#
+require 'bio'
+
+window_size = ARGV[1].to_i
+window_size = 5000 if window_size == nil or window_size < 1
+#puts "track type=wiggle_0 name=gc_content description=gc_content_5kb_win"
+
+ff = Bio::FlatFile.open(nil,ARGV[0])
+ff.each do |fe|
+  cur_win = 1
+  puts "fixedStep chrom=#{fe.acc_version} start=#{cur_win} step=#{window_size}"
+  fe.naseq.window_search(window_size,window_size) do |winseq|
+    puts winseq.gc_content.to_f * 100
+    cur_win += window_size   
+  end
+end
+
